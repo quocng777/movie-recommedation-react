c949cd7ca1d9efe8c0d589e43474d6b5653c335a
Quoc Nguyen <quocng777@gmail.com>
2024-12-09 13:19:34 +0700

    feat: add header

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -3,15 +3,8 @@ import { Button } from "@/components/ui/button";
 import { useDispatch, useSelector } from "react-redux"
 
 export const Homepage = () => {
-    const authenticatedUser = useSelector(getCurrentAuthentication);
-    const dispatch = useDispatch();
-
-    const onLogoutClick = () => {
-        dispatch(logOut())
-    }
 
     return <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
-        <h1 className="text-4xl">{'Welcome ' + authenticatedUser.fullname!}</h1>
-        <Button onClick={onLogoutClick}> Logout </Button>
+        home page
     </div>
 }
