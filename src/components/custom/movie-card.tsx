d0193e1d209ee40b31e4bc4aa0a3337238721026
Quoc Nguyen <quocng777@gmail.com>
2024-12-23 16:53:09 +0700

    feat: movie actions ui

--- src/components/custom/movie-card.tsx
+++ src/components/custom/movie-card.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { MouseEvent, MouseEventHandler, useState } from "react";
 import { Skeleton } from "../ui/skeleton";
 import { Movie } from "@/app/api/types/movie.type";
 import { getResourceFromTmdb } from "@/lib/helpers/get-resource-tmbd";
+import { Button } from "../ui/button";
+import { ThreeDotsVertical } from "react-bootstrap-icons";
+import MovieActionPopover from "./movie-action-popover";
 
 export type MovieCardProps = {
     movie: Movie;
@@ -17,12 +20,21 @@ export const MovieCard = (props: MovieCardProps) => {
     const onImageLoad = () => {
         setLoaded(true);
     };
+
+    const onMoreClick: MouseEventHandler = (e: MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+    };
     
     return (
         <div className="rounded-lg overflow-hidden cursor-pointer shrink-0 w-40" onClick={onClick}>
-            <div className="w-40 relative h-[15rem]">
-                <img src={getResourceFromTmdb(movie.poster_path)} onLoad={onImageLoad} className={`${!loaded ? 'opacity-0' : ''}`}/>
+            <div className="w-40 relative h-[15rem] group">
+                <img src={getResourceFromTmdb(movie.poster_path)} onLoad={onImageLoad} className={`${!loaded ? 'opacity-0' : 'group-hover:blur-sm'}`}/>
                 {!loaded && <Skeleton className="top-0 absolute bottom-0 right-0 left-0 opacity-100" />}
+                <MovieActionPopover>
+                    <Button variant="ghost" size="icon" className="rounded-full shrink-0 absolute top-2 right-2" onClick={onMoreClick}>
+                        <ThreeDotsVertical />
+                    </Button>
+                </MovieActionPopover>
             </div>
             <div className="mt-2 flex flex-col">
                 <span className="font-semibold text-ellipsis line-clamp-1">{movie.original_title}</span>
---
+++ src/constants/movies.ts
@@ -0,0 +1,5 @@
+export enum MovieAction {
+    'WATCH_LIST' = 'watch list',
+    'MY_LIST' = 'my list',
+    'LIKE_LIST' = 'like list',
+};
